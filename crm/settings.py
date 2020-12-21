b8cd72ca1966eede9f6b1efa653e2a822e3f278c
Shabix <kmharouna@gmail.com>
2020-12-21 16:25:01 +0300

    fixed the routing of static files

--- crm/settings.py
+++ crm/settings.py
@@ -118,5 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
